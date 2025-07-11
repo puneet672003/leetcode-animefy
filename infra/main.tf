f896ee61c22386315f215f5f8f6180d239846a38
unknown <puneetkpoonia672003@gmail.com>
2025-07-11 22:13:19 +0530

    fixed deploy workflow, fixed routing issue in lamnbda

--- infra/main.tf
+++ infra/main.tf
@@ -73,7 +73,7 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
 
 resource "aws_apigatewayv2_route" "default_route" {
   api_id = aws_apigatewayv2_api.http_api.id
-  route_key = "ANY /"
+  route_key = "ANY /{proxy+}"
   target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
